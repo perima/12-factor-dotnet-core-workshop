98a558a3b40cb7a2f38b82b4ed481e7d8155ad64
Mostafa Mabrouk <mosmabro@amazon.com>
2020-06-08 21:58:31 +1000

    fixing cloud9 setup script

--- scripts/cloud9setup.sh
+++ scripts/cloud9setup.sh
@@ -4,11 +4,9 @@ sudo yum -y update
 sudo yum -y install libunwind
 sudo apt -y update
 pip install awscli --upgrade --user
-npm uninstall -g aws-sam-local
-rm `which sam`
-pip install aws-sam-cli --user
+pip install aws-sam-cli --upgrade --user
 ln -sfn $(which sam) ~/.c9/bin/sam
-curl -O https://dot.net/v1/dotnet-install.sh
+curl -s -L https://dot.net/v1/dotnet-install.sh -O
 sudo chmod u=rx dotnet-install.sh
 ./dotnet-install.sh -c Current
 export PATH=$PATH:$HOME/.local/bin:$HOME/bin:$HOME/.dotnet
